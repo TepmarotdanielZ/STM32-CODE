1976a78b1ce11f70604085e464020667fe08c0ed
TepmarotdanielZ <lukasdaniel050403@gmail.com>
2025-02-12 14:36:05 +0700

    F401_External_RotaryEncoder

--- F401_External_RotaryEncoder/Core/Src/main.c
+++ F401_External_RotaryEncoder/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -43,15 +43,15 @@
 
 /* USER CODE BEGIN PV */
 
-	#define ENCODER_M_L1_Pin GPIO_PIN_6
-	#define ENCODER_M_R1_Pin GPIO_PIN_7
-	#define ENCODER_M_L1_GPIO_Port GPIOA
-	#define ENCODER_M_R1_GPIO_Port GPIOA
-	int encoder1;
-	int encoder2;
-	int counter = 0;
-	int last_encoder1 = 0;
-	int last_encoder2 = 0;
+#define ENCODER_M_L1_Pin GPIO_PIN_6
+#define ENCODER_M_R1_Pin GPIO_PIN_7
+#define ENCODER_M_L1_GPIO_Port GPIOA
+#define ENCODER_M_R1_GPIO_Port GPIOA
+int encoder1;
+int encoder2;
+int counter = 0;
+int last_encoder1 = 0;
+int last_encoder2 = 0;
 
 /* USER CODE END PV */
 
@@ -59,14 +59,14 @@
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
 
-	/* EXTERNAL ENTERRUPT */
+/* EXTERNAL ENTERRUPT */
 
-		void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
-		  if(GPIO_Pin == ENCODER_M_L1_Pin||ENCODER_M_R1_Pin){
-			encoder1 = HAL_GPIO_ReadPin(ENCODER_M_L1_GPIO_Port, ENCODER_M_L1_Pin);
-			encoder2 = HAL_GPIO_ReadPin(ENCODER_M_R1_GPIO_Port, ENCODER_M_R1_Pin);
-		  }
-		}
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+	if (GPIO_Pin == ENCODER_M_L1_Pin || ENCODER_M_R1_Pin) {
+		encoder1 = HAL_GPIO_ReadPin(ENCODER_M_L1_GPIO_Port, ENCODER_M_L1_Pin);
+		encoder2 = HAL_GPIO_ReadPin(ENCODER_M_R1_GPIO_Port, ENCODER_M_R1_Pin);
+	}
+}
 
 /* USER CODE END PFP */
 
@@ -76,133 +76,128 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+	/* USER CODE BEGIN 1 */
 
 	/* READ ENCODER */
 
-		void encoder(){
-				  if(encoder1 != last_encoder1){
-					last_encoder1 = encoder1;
-					if(encoder1 == 0){
-					  if(encoder2 == 1){
-
-						counter++;
-					  }else{
-						counter--;
-					  }
-					}else{
-					  if(encoder2 == 0){
-						counter++;
-					  }else{
-						counter--;
-					  }
-					}
-				  }
-				  if(encoder2 != last_encoder2){
-					last_encoder2 = encoder2;
-					if(encoder2 == 1){
-					  if(encoder1 == 1){
-						counter++;
-					  }else{
-						counter--;
-					  }
-					}else{
-					  if(encoder1 == 0){
-						counter++;
-					  }else{
-						counter--;
-					  }
-					}
-				  }
+	void encoder() {
+		if (encoder1 != last_encoder1) {
+			last_encoder1 = encoder1;
+			if (encoder1 == 0) {
+				if (encoder2 == 1) {
+					counter++;
+				} else {
+					counter--;
 				}
+			} else {
+				if (encoder2 == 0) {
+					counter++;
+				} else {
+					counter--;
+				}
+			}
+		}
+		if (encoder2 != last_encoder2) {
+			last_encoder2 = encoder2;
+			if (encoder2 == 1) {
+				if (encoder1 == 1) {
+					counter++;
+				} else {
+					counter--;
+				}
+			} else {
+				if (encoder1 == 0) {
+					counter++;
+				} else {
+					counter--;
+				}
+			}
+		}
+	}
 
 
-  /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* USER CODE END 1 */
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-   HAL_Init();
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* USER CODE BEGIN Init */
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE END Init */
+	/* USER CODE BEGIN Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* USER CODE END Init */
 
-  /* USER CODE BEGIN SysInit */
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE END SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
+	/* USER CODE END SysInit */
 
-  /* USER CODE END 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	/* USER CODE BEGIN 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
+	/* USER CODE END 2 */
 
-    /* USER CODE BEGIN 3 */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1) {
+		/* USER CODE END WHILE */
 
-	  	  encoder();
+		/* USER CODE BEGIN 3 */
 
-  }
-  /* USER CODE END 3 */
+		encoder();
+
+	}
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 25;
-  RCC_OscInitStruct.PLL.PLLN = 168;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 25;
+	RCC_OscInitStruct.PLL.PLLN = 168;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -210,18 +205,16 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
